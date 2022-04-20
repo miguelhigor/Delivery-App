1761abd2a55970cd9e088b0ee2be7ee7900ab490
Higor Miguel <higor.mig@gmail.com>
2022-04-19 22:39:59 -0300

    feat: error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, response, Response } from "express";
 import dotenv from "dotenv";
+import "express-async-errors";
 
 import { routes } from "./routes";
 
@@ -9,7 +10,23 @@ if (process.env.NODE_ENV !== 'production') {
 
 const port = process.env.SERVER_PORT;
 const app = express();
+
 app.use(express.json());
 app.use(routes);
+
+app.use(
+    (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (err instanceof Error) {
+            return res.status(400).json({
+                message: err.message,
+            });
+        }
+
+        return res.status(500).json({
+            status: "Error",
+            message: "Internal Server Error",
+        });
+    }
+)
 
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
